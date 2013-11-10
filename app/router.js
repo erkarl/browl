33f3fae7b959954b6a313d40ed4a198824e084e3
erkarl <karl@karlranna.com>
2013-11-10 06:34:47 +0200

    Added routes and templates for post management.

--- app/router.js
+++ app/router.js
@@ -3,6 +3,11 @@ var Router = Ember.Router.extend(); // ensure we don't share routes between all 
 Router.map(function() {
   this.resource('post', { path: "/post/:id" }, function(){
   });
+
+  this.resource('admin', function(){
+      this.route('new');
+  });
+
 });
 
 export default Router;
