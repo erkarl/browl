e26e67431ef3f5c08c518e67725cec57c2a8dff4
erkarl <karl@karlranna.com>
2013-11-12 14:53:30 +0200

    Switched to localstorage adapter.

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,2 +1,10 @@
+/*
 var FixtureAdapter = DS.FixtureAdapter.extend();
 export default FixtureAdapter;
+*/
+
+var ApplicationAdapter = DS.LSAdapter.extend({
+  namespace: 'browl'
+});
+
+export default ApplicationAdapter;
