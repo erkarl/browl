f11c76f7eaac53c9f30e9fb720823fb38eb80247
erkarl <karl@karlranna.com>
2013-11-12 21:38:27 +0200

    Build fix pt1.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,8 +1,6 @@
 var IndexController = Ember.ArrayController.extend({
     actions: {
         deleteEntry: function(post){
-          console.log('deleteEntry action' + post);
-          console.log('postID' + post.get('id'));
           post.deleteRecord();
           post.save();
         }
