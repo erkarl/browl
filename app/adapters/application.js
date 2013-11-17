67337b6f508058e23133762b5af7b704e69d006a
erkarl <karl@karlranna.com>
2013-11-17 13:14:13 +0200

    Added django-rest-adapter.

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,8 +3,15 @@ var FixtureAdapter = DS.FixtureAdapter.extend();
 export default FixtureAdapter;
 */
 
+/*
 var ApplicationAdapter = DS.LSAdapter.extend({
   namespace: 'browl'
+});
+
+export default ApplicationAdapter;
+*/
+var ApplicationAdapter = DS.DjangoRESTAdapter.extend({
+  host: 'http://localhost:7000'
 });
 
 export default ApplicationAdapter;
