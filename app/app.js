ecf0445485ea2025289c360dafb289c8c204635e
erkarl <karl@karlranna.com>
2013-12-12 20:31:10 +0200

    Added authentication initializer.

--- app/app.js
+++ app/app.js
@@ -10,9 +10,25 @@ var App = Ember.Application.extend({
   Resolver: Resolver['default']
 });
 
+App.initializer({
+  name: 'Register Components',
+  initialize: function(container, application) {
+    registerComponents(container);
+    // Options for Auth
+    var options = {
+        serverTokenEndpoint: "http://browl-api.karlranna.com/oauth2/access_token/"
+    };
+    Ember.SimpleAuth.setup(container, application, options);
+  }
+});
+
 Ember.RSVP.configure('onerror', function(error) {
   // ensure unhandled promises raise awareness.
   // may result in false negatives, but visibility is more important
+  var options = {
+      serverTokenEndpoint: "http://browl-api.karlranna.com/oauth2/access_token/"
+  };
+  Ember.SimpleAuth.setup(container, application, options);
   if (error instanceof Error) {
     Ember.Logger.assert(false, error);
     Ember.Logger.error(error.stack);
--- app/controllers/admin/new.js
+++ app/controllers/admin/new.js
@@ -13,10 +13,43 @@ var NewAdminController = Ember.Controller.extend({
         });
     },
 
+    address_id: null,
+
+    savePost: function(userModel){
+      console.log('saving new post');
+      console.log('userModel: ' + userModel);
+      this.set('address_id', 123);
+      /*
+      this.get('store').find('user', 1).then(function(userModel){
+        var addnew = that;
+        var hash = {
+          user: userModel,
+          title: that.get('title'),
+          body: that.get('body')
+        };
+        // console.log('hash: ' + JSON.stringify(hash));
+        addnew.store.createRecord('post', hash).save().then(function(){
+          addnew.reset();
+          addnew.transitionToRoute('index');
+        });
+      });
+      */
+    },
+
+    somethingElse: function(addressID){
+      console.log('somethingElse');
+      console.log('CONTROLLERS VARIABLE ADDRESS ID: ' + this.get('address_id'));
+    },
+
     actions: {
         addNew: function(){
           var hash = this.getProperties('title', 'body');
-          var that = this;
+          var context = this;
+          this.get('store').find('user', 1).
+            then(this.savePost()).
+            then(this.somethingElse());
+
+          /* OLD
           this.get('store').find('user', 1).then(function(userModel){
             var addnew = that;
             var hash = {
@@ -30,6 +63,7 @@ var NewAdminController = Ember.Controller.extend({
               addnew.transitionToRoute('index');
             });
           });
+          */
         }
     }
 });
