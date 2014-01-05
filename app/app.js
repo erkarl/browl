503a398786732d6fda89e8df5b1cf54c41b1106f
erkarl <karl@karlranna.com>
2014-01-05 20:38:15 +0200

    Fix ember-simple-auth init

--- app/app.js
+++ app/app.js
@@ -10,15 +10,20 @@ var App = Ember.Application.extend({
   Resolver: Resolver['default']
 });
 
+App.initializer({
+  name: 'Register Components',
+  initialize: function(container, application) {
+    // Options for Auth
+    var options = {
+        serverTokenEndpoint: "http://karrancia.com/oauth2/access_token/"
+    };
+    Ember.SimpleAuth.setup(container, application, options);
+  }
+});
 
 Ember.RSVP.configure('onerror', function(error) {
   // ensure unhandled promises raise awareness.
   // may result in false negatives, but visibility is more important
-  var options = {
-      serverTokenEndpoint: "http://browl-api.karlranna.com/oauth2/access_token/"
-  };
-  Ember.SimpleAuth.setup(container, application, options);
-  console.log('ember simpleauth setup');
   if (error instanceof Error) {
     Ember.Logger.assert(false, error);
     Ember.Logger.error(error.stack);
